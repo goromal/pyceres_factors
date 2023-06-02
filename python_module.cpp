9129572645a1f999cb41e21951d701fa672f4c76
Andrew Torgesen <andrew.torgesen@gmail.com>
2023-06-01 23:02:19 -0700

    fix formatting

--- python_module.cpp
+++ python_module.cpp
@@ -11,30 +11,31 @@
 using namespace Eigen;
 namespace py = pybind11;
 
-PYBIND11_MODULE(PyCeresFactors, m) {
-  m.doc() = "Python binding module for custom Ceres factors.";
+PYBIND11_MODULE(PyCeresFactors, m)
+{
+    m.doc() = "Python binding module for custom Ceres factors.";
 
-  // SO2 Factors
-  m.def("SO2Parameterization", &SO2Parameterization::Create);
+    // SO2 Factors
+    m.def("SO2Parameterization", &SO2Parameterization::Create);
 
-  // SE2 Factors
-  m.def("SE2Parameterization", &SE2Parameterization::Create);
+    // SE2 Factors
+    m.def("SE2Parameterization", &SE2Parameterization::Create);
 
-  // SO3 Factors
-  m.def("SO3Parameterization", &SO3Parameterization::Create);
-  m.def("SO3Factor", &SO3Factor::Create);
+    // SO3 Factors
+    m.def("SO3Parameterization", &SO3Parameterization::Create);
+    m.def("SO3Factor", &SO3Factor::Create);
 
-  // SE3 Factors
-  m.def("SE3Parameterization", &SE3Parameterization::Create);
-  m.def("RelSE3Factor", &RelSE3Factor::Create);
+    // SE3 Factors
+    m.def("SE3Parameterization", &SE3Parameterization::Create);
+    m.def("RelSE3Factor", &RelSE3Factor::Create);
 
-  // Specific Sensor Factors
-  m.def("RangeFactor", &RangeFactor::Create);
-  m.def("AltFactor", &AltFactor::Create);
-  m.def("RangeBearing2DFactor", &RangeBearing2DFactor::Create);
+    // Specific Sensor Factors
+    m.def("RangeFactor", &RangeFactor::Create);
+    m.def("AltFactor", &AltFactor::Create);
+    m.def("RangeBearing2DFactor", &RangeBearing2DFactor::Create);
 
-  // Calibration Factors
-  m.def("TimeSyncAttFactor", &TimeSyncAttFactor::Create);
-  m.def("SO3OffsetFactor", &SO3OffsetFactor::Create);
-  m.def("SE3OffsetFactor", &SE3OffsetFactor::Create);
+    // Calibration Factors
+    m.def("TimeSyncAttFactor", &TimeSyncAttFactor::Create);
+    m.def("SO3OffsetFactor", &SO3OffsetFactor::Create);
+    m.def("SE3OffsetFactor", &SE3OffsetFactor::Create);
 }
